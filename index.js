45832e7eeee542d6cf98ef0b8ccad37b3d31ca84
Atabek <aakmataliev04@gmail.com>
2024-11-28 17:49:34 +0600

    Installed caesar salad, wrote code for encoding text params in '/encode/:text' and render encoded text

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const Vigenere = require('caesar-salad').Vigenere;
 
 const app = express();
 const port = 8000;
@@ -9,7 +10,10 @@ app.get('/', (req, res) => {
 app.get('/:text', (req, res) => {
     return res.send(`${req.params.text}`);
 })
-
+app.get('/encode/:text', (req, res) => {
+    const cipher = Vigenere.Cipher('password').crypt(`${req.params.text}`);
+    return res.send(cipher);
+})
 
 app.listen(port, () => {
     console.log(`Server running on port http://localhost:${port}`);
