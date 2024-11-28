7dcf2e915f09e0a34b3f8fe41dba45b24d9e1a42
Atabek <aakmataliev04@gmail.com>
2024-11-28 17:39:13 +0600

    First task is done it gives response for any text

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ const port = 8000;
 app.get('/', (req, res) => {
     return res.send('Home Page');
 });
+app.get('/:text', (req, res) => {
+    return res.send(`${req.params.text}`);
+})
+
 
 app.listen(port, () => {
     console.log(`Server running on port http://localhost:${port}`);
